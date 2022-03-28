b710137a110568df8b0365bb3a3f554f7c91f554
Dmitry Khmara <dmitry.khmara@gmail.com>
2022-03-28 22:31:54 +0300

    Add a simplistic StringEnum class that allows to define members and access their values.

---
+++ test/StrEnum.UnitTests/DefineTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace StrEnum.UnitTests;
+
+
+public class DefineTests
+{
+    private class ChannelWithTwoDifferentKeys : StringEnum<ChannelWithTwoDifferentKeys>
+    {
+        public static readonly ChannelWithTwoDifferentKeys Sms = Define("SMS");
+        public static readonly ChannelWithTwoDifferentKeys Email = Define("Email");
+    }
+
+    [Fact]
+    public void Define_GivenDifferentKeys_ShouldCreateEnum()
+    {
+        ChannelWithTwoDifferentKeys.Sms.ToString().Should().Be("SMS");
+        ChannelWithTwoDifferentKeys.Email.ToString().Should().Be("Email");
+    }
+
+    private class ChannelWithAMemberValueOfNull : StringEnum<ChannelWithAMemberValueOfNull>
+    {
+        public static readonly ChannelWithAMemberValueOfNull Sms = Define(null);
+    }
+
+    [Fact]
+    public void Define_GivenAValueOfNull_ShouldThrowAnException()
+    {
+        var accessMember = () => ChannelWithAMemberValueOfNull.Sms;
+        
+        accessMember.Should().Throw<TypeInitializationException>()
+            .WithInnerException<ArgumentNullException>()
+            .WithParameterName("value");
+    }
+
+    private class ChannelWithAMemberNameOfNull : StringEnum<ChannelWithAMemberNameOfNull>
+    {
+        public static readonly ChannelWithAMemberNameOfNull Sms = Define("SMS", null);
+    }
+
+    [Fact]
+    public void Define_GivenANameOfNull_ShouldThrowAnException()
+    {
+        var accessMember = () => ChannelWithAMemberNameOfNull.Sms;
+
+        accessMember.Should().Throw<TypeInitializationException>()
+            .WithInnerException<ArgumentNullException>()
+            .WithParameterName("name");
+    }
+}
