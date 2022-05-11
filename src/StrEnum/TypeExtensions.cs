36f5df9acc3574f0772141fc314151338e0bb29b
Dmitry Khmara <dmitry.khmara@gmail.com>
2022-05-11 22:01:37 +0300

    Add the `IsStringEnum` extension method. (#6)
    Add the `IsStringEnum` extension method for the `Type` type which checks whether the given type is a string enum. (#6)

---
+++ src/StrEnum/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StrEnum
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Gets a values indicating whether the type is a StrEnum string enum.
+        /// </summary>
+        /// <param name="type">The type to check</param>
+        /// <returns></returns>
+        public static bool IsStringEnum(this Type type)
+        {
+            if (!type.IsClass) return false;
+
+            var baseType = type.BaseType;
+
+            if (baseType == null) return false;
+
+            var baseClassIsStringEnumGeneric = baseType.IsGenericType && baseType.GetGenericTypeDefinition() == typeof(StringEnum<>);
+
+            return baseClassIsStringEnumGeneric;
+        }
+    }
+}
---
+++ test/StrEnum.UnitTests/IsStringEnumTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace StrEnum.UnitTests;
+
+public class IsStringEnumTests
+{
+    public class Sports : StringEnum<Sports>
+    {
+    }
+
+    public static IEnumerable<object?[]> IsStringEnumTestCases =>
+        new[]
+        {
+            new object?[] { typeof(int), false },
+            new object?[] { typeof(string), false },
+            new object?[] { typeof(object), false },
+            new object?[] { typeof(List<string>), false },
+            new object?[] { typeof(StringEnum<Sports>), false },
+            new object?[] { typeof(Sports), true },
+        };
+
+    [Theory]
+    [MemberData(nameof(IsStringEnumTestCases))]
+    public void IsStringEnum_GivenAType_ShouldCheckWhetherItIsAStringEnum(Type type, bool isStringEnum)
+    {
+        type.IsStringEnum().Should().Be(isStringEnum);
+    }
+}
