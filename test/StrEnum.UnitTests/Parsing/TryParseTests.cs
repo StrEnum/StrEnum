7c037e611de51fadcbbb5d79ff0f1d388945a57b
Dmitry Khmara <dmitry.khmara@gmail.com>
2022-07-29 16:37:44 +0300

    Add a `MatchBy` parameter to the `Parse` and `TryParse` methods and reorganize `TryParse` parameters.

--- test/StrEnum.UnitTests/Parsing/TryParseTests.cs
+++ test/StrEnum.UnitTests/Parsing/TryParseTests.cs
@@ -15,21 +15,21 @@ public class TryParseTests
     [InlineData("TRAIL_RUNNING", false)]
     [InlineData("trailrunning", true)]
     [InlineData("trail_running", true)]
-    public void TryParse_GivenAMatchingMemberByNameOrValue_ShouldFindThatMemberAndReturnTrue(
+    public void TryParse_GivenAMatchingMember_ByNameOrValue_ShouldFindThatMemberAndReturnTrue(
         string nameOrValue, bool ignoreCase)
     {
-        var parsed = Sport.TryParse(nameOrValue, ignoreCase, out var trailRunning);
+        var parsed = Sport.TryParse(nameOrValue, out var trailRunning, ignoreCase);
 
         parsed.Should().BeTrue();
         trailRunning.Should().Be(Sport.TrailRunning);
     }
 
     [Fact]
-    public void TryParse_GivenAValueThatDoesntMatchAMember_ShouldReturnFalseAndSetMemberToNull()
+    public void TryParse_GivenAValueThatDoesntMatchAMember_ByNameOrValue_ShouldReturnFalseAndSetMemberToNull()
     {
         var trailRunning = Sport.TrailRunning;
 
-        var parsed = Sport.TryParse("TRAIL", true, out trailRunning);
+        var parsed = Sport.TryParse("TRAIL", out trailRunning, true);
 
         parsed.Should().BeFalse();
         trailRunning.Should().BeNull();
